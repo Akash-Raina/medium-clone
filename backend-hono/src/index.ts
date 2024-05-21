c4679ebb66bc420a6b1447033889a8242f543e3e
Akash Raina <kevinraina279@gmail.com>
2024-05-21 15:41:07 +0530

    reuploaded backend code + common module

---
+++ backend-hono/src/index.ts
@@ -0,0 +1,12 @@
+import { Hono } from 'hono'
+import { userRouter } from './router/userRouter';
+import { blogRouter } from './router/blogRouter';
+
+
+const app = new Hono()
+app.route('/api/v1/user', userRouter);
+app.route('/api/v1/blog', blogRouter);
+
+
+
+export default app
