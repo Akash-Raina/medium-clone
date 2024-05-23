346ca077252a47e89d9cab012c2e7413ec56ebae
Akash Raina <kevinraina279@gmail.com>
2024-05-23 21:10:24 +0530

    added allblogs component

---
+++ frontend/src/components/Allblogs.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+
+export const Allblogs =({title, id}:{title:string, id:number})=>{
+    return <Link to={`/blog/${id}`}>
+        <div className="flex mb-5 text-slate-600 hover:text-slate-900  hover:border-l-4 hover:border-l-blue-700 hover:translate-x-6 hover:duration-300 text-2xl font-semibold cursor-pointer">
+        {title}
+        </div>
+    </Link>
+}
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,6 +1,7 @@
 import { BlogCard } from "../components/BlogCard"
 import { Appbar } from "../components/Appbar"
 import { UseBlocks } from "../hooks"
+import { Allblogs } from "../components/Allblogs";
 
 export const Blogs = ()=>{
     
@@ -10,21 +11,24 @@ export const Blogs = ()=>{
                 loading...
             </div>
         }
-    return <div className="">
+    return <>
 
         <div className="border-b">
             <Appbar/>
         </div>
 
-        <div className="flex pt-6">
-            <div className=" lg:px-12 lg:pt-2 lg:w-[60%]">
+        <div className="flex pt-6 border">
+            <div className="lg:pt-2 lg:w-[65%] lg:px-24">
                 {blogs.map(blog => <BlogCard id={blog.id}
-        authorName = {blog.author.name || "Anonymous"} title ={blog.title} publishedDate = "Apr 22, 2024" content = {blog.content}
-        />)}
+                authorName = {blog.author.name || "Anonymous"} title ={blog.title} publishedDate = "Apr 22, 2024" content = {blog.content}
+                />)}
             
-        </div>
-        <div className="hidden lg:block lg:w-[35%] lg:pl-2 lg:border-l">Blog list</div>
+            </div>
+            <div className="hidden lg:flex lg:flex-col lg:gap-2 lg:w-[34%] lg:border-l lg:px-12">
+                <div className="border-b p-4 text-xl text-slate-800 text-center font-semibold">All blogs</div>
+                {blogs.map(blog => <Allblogs title ={blog.title} id={blog.id}/>) }
+            </div>
         </div>
         
-    </div>
+    </>
 }
