53ebb0945f1000c1d1bcd58f4fa0a8e8a5496787
Akash Raina <kevinraina279@gmail.com>
2024-06-08 14:03:27 +0530

    fixed to resize bug

--- frontend/src/components/Authheader.tsx
+++ frontend/src/components/Authheader.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom"
 export const Authheader = ({type}:{type: "signup" | "signin"})=>{
 
-    return <div>
-        <span className="text-2xl lg:mx-14 text-center font-bold lg:text-3xl">{type=== "signup"? "Create an account": "Login Credentials"}</span>
+    return <div className="flex flex-col items-center">
+        <span className="border text-2xl lg:mx-14 font-bold lg:text-3xl">{type=== "signup"? "Create an account": "Login Credentials"}</span>
 
             <div className="lg:text-center text-slate-500">
                 {type === "signup" ? "Already have an account?": "create new account?"}
--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -1,5 +1,5 @@
 export const Spinner = ()=>{
-    return <div className="mx-44 animate-spin rounded-full h-6 w-6 border-t-4 border-b-4 border-blue-500">
-
+    return <div className="flex justify-center">
+        <div className="animate-spin rounded-full h-6 w-6 border-t-4 border-b-4 border-blue-500"></div>
     </div>
 }
