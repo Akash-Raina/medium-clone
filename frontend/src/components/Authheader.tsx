eabd79359ed91d98ca7b7efe48c6ed210fb4c837
Akash Raina <kevinraina279@gmail.com>
2024-06-08 13:26:12 +0530

    added loadingSpinner in signin/up pages

--- frontend/src/components/Authheader.tsx
+++ frontend/src/components/Authheader.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 export const Authheader = ({type}:{type: "signup" | "signin"})=>{
 
     return <div>
-        <span className="mx-14 text-center font-bold text-3xl">{type=== "signup"? "Create an account": "Login Credentials"}</span>
+        <span className="text-2xl lg:mx-14 text-center font-bold lg:text-3xl">{type=== "signup"? "Create an account": "Login Credentials"}</span>
 
-            <div className="text-center text-slate-500">
+            <div className="lg:text-center text-slate-500">
                 {type === "signup" ? "Already have an account?": "create new account?"}
                 <Link to={type === "signup"? "/signin": "/signup"} className="underline pl-1">{type === "signup"? "Login": "Signup"}</Link>
             </div>
--- frontend/src/components/SigninAuth.tsx
+++ frontend/src/components/SigninAuth.tsx
@@ -3,23 +3,28 @@ import { ChangeEvent, useState } from "react";
 import { Authheader } from "./Authheader";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
+import { Spinner } from "./Spinner";
 import { useNavigate } from "react-router-dom";
 
 export const SigninAuth =({type}: {type: "signup" |"signin"})=>{
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
     const [postInputs, setpostInputs] = useState<signinType>({
         email: "",
         password: ""
     })
     async function hitbackend(){
         try{
+            setLoading(true)
             const response = await axios.post(`${BACKEND_URL}/api/v1/user/signin`, postInputs);
+            setLoading(false);
             const jwt = response.data;
             localStorage.setItem("token", jwt);
             navigate('/blogs');
         }
         catch(e){
             alert("Wrong inputs");
+            setLoading(false)
         }
     }
     return <div className="flex flex-col h-screen bg-slate-100 justify-center">
@@ -41,7 +46,7 @@ export const SigninAuth =({type}: {type: "signup" |"signin"})=>{
                     })
                 }}/>
 
-                <button onClick={hitbackend} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm py-2.5  mb-2 my-4">{type === "signup" ? "Sign Up" : "Sign In"}</button>
+                <button onClick={hitbackend} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm py-2.5  mb-2 my-4">{loading === false ? type === "signup" ? "Sign Up" : "Sign In" : <Spinner/>}</button>
             </div>
         </div>
     </div>
