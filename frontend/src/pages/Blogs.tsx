cf76f67fccb23426a02d31b8a9c7f24a347a5318
Akash Raina <kevinraina279@gmail.com>
2024-06-08 14:15:02 +0530

    added changes

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -14,11 +14,11 @@ export const Blogs = ()=>{
         }
     return <>
 
-        <div className="border-b">
+        <div className="border-b fixed w-screen bg-white">
             <Appbar/>
         </div>
 
-        <div className="flex pt-6 border">
+        <div className="flex pt-16 border">
             <div className="px-20 lg:pt-2 lg:w-[65%] lg:px-24 ">
                 {blogs.map(blog => <BlogCard id={blog.id}
                 authorName = {blog.author.name || "Anonymous"} title ={blog.title} publishedDate = "Apr 22, 2024" content = {blog.content}
